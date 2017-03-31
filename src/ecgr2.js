814850aa0c1e2507db02676da5f046c5172abad3
DanielLeah <daniel.leah94@gmail.com>
2017-03-31 19:53:01 +0300

    cod cu functii~!

--- src/ecgr2.js
+++ src/ecgr2.js
@@ -1,35 +1,78 @@
-function rezolva()
+function citeste()
 {
 	var a=document.getElementById("a").value;
 	var b=document.getElementById("b").value;
 	var c=document.getElementById("c").value;
-	var delta = (b*b) - (4*a*c);
-	document.getElementById("delta").innerHTML =delta;
-	var x1,x2, x1_im,x2_im;
-	if (delta==0)
+	
+	var coef=[a,b,c];
+	return coef;
+}
+
+//----------------------------------------------------------------------
+function calcul_delta(coeficienti)
+{
+	
+	return coeficienti[1]*coeficienti[1]-4*coeficienti[0]*coeficienti[2];
+}
+
+//----------------------------------------------------------------------
+function afiseaza(radacini){
+if (radacini[3]>=0)
+{
+document.getElementById("x2").innerHTML =radacini[2].toString()+"+"+radacini[3].toString()+"i";;
+}
+else{
+document.getElementById("x2").innerHTML =radacini[2].toString()+radacini[3].toString()+"i";	
+}
+if(radacini[1]>0){
+	document.getElementById("x1").innerHTML =radacini[0].toString()+"+"+radacini[1].toString()+"i";
+}
+else{
+document.getElementById("x1").innerHTML =radacini[0].toString()+"+"+radacini[1].toString()+"i";
+}
+}
+
+//----------------------------------------------------------------------
+
+function calcul_radacini(coef,delta){
+var x1,x1_im,x2,x2_im;
+if (delta==0)
 	{
-	x1=x2=(-b/(2*a));
-	document.getElementById("x1").innerHTML =x1;
-	document.getElementById("x2").innerHTML =x2;
+	x1=x2=(-coef[1]/(2*coef[0]));
+	x1_im=x2_im=0;
 	}
 	else
 	{
 		if(delta>0)
 		{
-			x1=(-b+Math.sqrt(delta))/2*a;
-			x2=(-b-Math.sqrt(delta))/2*a;
-			document.getElementById("x1").innerHTML =x1;
-			document.getElementById("x2").innerHTML =x2;
+			x1=(-coef[1]+Math.sqrt(delta))/2*coef[0];
+			x2=(-coef[1]-Math.sqrt(delta))/2*coef[0];
+			x1_im=x2_im=0;
 		}
 		else
 		{
-		    x1=(-b)/2*a;
-			x1_im=Math.sqrt(-delta)/2*a;
-			x2=(-b)/2*a;
-			x2_im=-Math.sqrt(-delta)/2*a;
-			document.getElementById("x1").innerHTML =x1.toString()+"+"+x1_im.toString()+"i";
-			document.getElementById("x2").innerHTML =x2.toString()+x2_im.toString()+"i";;
+		    x1=(-coef[1])/2*coef[0];
+			x1_im=Math.sqrt(-delta)/2*coef[0];
+			x2=(-coef[1])/2*coef[0];
+			x2_im=-Math.sqrt(-delta)/2*coef[0];
+			
 		}
 	}
+
+
+return [x1, x1_im, x2, x2_im];
 }
 
+//----------------------------------------------------------------------
+
+function rezolva()
+{
+	var coef=citeste();
+	var delta = calcul_delta(coef);
+	document.getElementById("delta").innerHTML =delta;
+	var radacini=calcul_radacini(coef,delta);
+	afiseaza(radacini);
+	
+}
+
+//----------------------------------------------------------------------
