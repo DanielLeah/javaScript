928471fa707991a221824d0bccb59ad42bd22610
DanielLeah <daniel.leah94@gmail.com>
2017-04-06 18:10:49 +0300

    Adaugare solutie structurata

--- src/ec2Struct.js
+++ src/ec2Struct.js
@@ -1,10 +1,10 @@
 function citeste()
 {
-	var a=document.getElementById("a").value;
-	var b=document.getElementById("b").value;
-	var c=document.getElementById("c").value;
+	var _a=document.getElementById("a").value;
+	var _b=document.getElementById("b").value;
+	var _c=document.getElementById("c").value;
 	
-	var coef=[a,b,c];
+	var coef={a:_a,b:_b,c:_c};
 	return coef;
 }
 
@@ -12,55 +12,51 @@ function citeste()
 function calcul_delta(coeficienti)
 {
 	
-	return coeficienti[1]*coeficienti[1]-4*coeficienti[0]*coeficienti[2];
+	return coeficienti.b*coeficienti.b-4*coeficienti.a*coeficienti.c;
 }
 
 //----------------------------------------------------------------------
 function afiseaza(radacini){
-if (radacini[3]>=0)
+if (radacini.sol2.im>=0)
 {
-document.getElementById("x2").innerHTML =radacini[2].toString()+"+"+radacini[3].toString()+"i";;
+document.getElementById("x2").innerHTML =radacini.sol2.re.toString()+"+"+radacini.sol2.im.toString()+"i";;
 }
 else{
-document.getElementById("x2").innerHTML =radacini[2].toString()+radacini[3].toString()+"i";	
+document.getElementById("x2").innerHTML =radacini.sol2.re.toString()+radacini.sol2.im.toString()+"i";	
 }
-if(radacini[1]>0){
-	document.getElementById("x1").innerHTML =radacini[0].toString()+"+"+radacini[1].toString()+"i";
+if(radacini.sol1.im>0){
+	document.getElementById("x1").innerHTML =radacini.sol1.re.toString()+"+"+radacini.sol1.im.toString()+"i";
 }
 else{
-document.getElementById("x1").innerHTML =radacini[0].toString()+"+"+radacini[1].toString()+"i";
+document.getElementById("x1").innerHTML =radacini.sol1.re.toString()+"+"+radacini.sol1.im.toString()+"i";
 }
 }
 
 //----------------------------------------------------------------------
 
 function calcul_radacini(coef,delta){
-var x1,x1_im,x2,x2_im;
+var x1,x2;
 if (delta==0)
 	{
-	x1=x2=(-coef[1]/(2*coef[0]));
-	x1_im=x2_im=0;
+	x1={re:-coef.b/(2*coef.a), im:0}
+	x2={re:-coef.b/(2*coef.a), im:0}
 	}
 	else
 	{
 		if(delta>0)
 		{
-			x1=(-coef[1]+Math.sqrt(delta))/2*coef[0];
-			x2=(-coef[1]-Math.sqrt(delta))/2*coef[0];
-			x1_im=x2_im=0;
+			x1={re:(-coef.b+Math.sqrt(delta))/2*coef.a, im:0}
+			x2={re:(-coef.b-Math.sqrt(delta))/2*coef.a, im:0}
 		}
 		else
 		{
-		    x1=(-coef[1])/2*coef[0];
-			x1_im=Math.sqrt(-delta)/2*coef[0];
-			x2=(-coef[1])/2*coef[0];
-			x2_im=-Math.sqrt(-delta)/2*coef[0];
-			
+			x1={re:(-coef.b)/2*coef.a, im:Math.sqrt(-delta)/2*coef.a}
+			x2={re:(-coef.b)/2*coef.a, im: -Math.sqrt(-delta)/2*coef.a}	
 		}
 	}
 
-
-return [x1, x1_im, x2, x2_im];
+var sol={sol1:x1, sol2:x2};
+return sol;
 }
 
 //----------------------------------------------------------------------
