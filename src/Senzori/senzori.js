9c556d967eff973dc7b084ebc62709d7a29c0193
DanielLeah <daniel.leah94@gmail.com>
2017-04-06 19:47:35 +0300

    Desenare cerc!

--- src/Senzori/senzori.js
+++ src/Senzori/senzori.js
@@ -4,4 +4,15 @@ function on_device_orientation(event)
 document.getElementById("x").innerHTML = "Alpha = "+event.alpha;
 document.getElementById("y").innerHTML = "Beta = "+event.beta;
 document.getElementById("z").innerHTML = "Gama = "+event.gamma;
+
+var canvas = document.getElementById("canvas");
+var context= canvas.getContext("2d");
+var raza = 20;
+var centru = {x:canvas.width/2, y:canvas.height/2};
+context.clearRect(0,0,canvas.width,canvas.height);
+
+context.beginPath();
+context.arc(centru.x+event.gamma*(canvas.width/2 -raza)/90,centru.y+event.beta*(canvas.height/2 -raza)/90,raza,0,2*Math.PI);
+context.stroke();
+
 }
