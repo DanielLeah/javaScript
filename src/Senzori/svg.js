09c88a122baa28bfde6bf4d5ead12aa88767da37
DanielLeah <daniel.leah94@gmail.com>
2017-05-04 19:19:51 +0300

    doua cercuri

---
+++ src/Senzori/svg.js
@@ -0,0 +1,16 @@
+var c1 = document.getElementById("cerc1");
+var c2 = document.getElementById("cerc2");
+c1.addEventListener("touchmove",on_touch_move);
+c2.addEventListener("touchmove",on_touch_move);
+
+function on_touch_move(e)
+{
+	for(var i=0;i<e.changedTouches.length;i++)
+	{
+		var t= e.changedTouches[i];
+		var c= t.target;
+		c.setAttribute("cx",t.pageX);
+		c.setAttribute("cy",t.pageY);
+	}	
+}
+
