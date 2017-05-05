7aca674f7e74eccd7668b6b96bed3c85711921ed
DanielLeah <daniel.leah94@gmail.com>
2017-05-05 18:39:01 +0300

    speech

---
+++ src/Speech/synt.js
@@ -0,0 +1,12 @@
+
+
+var synt = window.speechSynthesis;
+function on_get_voices()
+{
+var p=document.getElementById("voci");
+ var voices = synt.getVoices();
+ for (var i = 0; i<voices.length;i++)
+ {
+	 p.innerHTML+=voices[i].name+":"+voices[i].lang+"<br>";
+ }
+}
